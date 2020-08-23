679d309ff4e1bccfecb08510f4ee2240d94e8d15
Artem <bravo2694@gmail.com>
2020-08-23 15:54:11 +0300

    Working compare, need testing

--- gendiff/products/product_json.py
+++ gendiff/products/product_json.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 import json
+from itertools import tee
+from copy import copy
 
 from gendiff.generator_ast.generator_ast import GeneratorAST, Node
 
@@ -37,29 +39,53 @@ class JsonJSON(AbstractJSON):
         self.data = json.loads(data)
         return self.data
 
-    def compare(self, input_1, input_2):
-        gen = GeneratorAST()
-        root = gen.add_node(None, Node('root', []))
+    # TODO
+    def compare(self, input_1: GeneratorAST, input_2: GeneratorAST) -> GeneratorAST:
+        diff_tree = GeneratorAST()
+        root = diff_tree.add_node(None, Node('root', []))
 
-        for value_1, value_2 in zip(self.research(input_1), self.research(input_2)):
-            if value_1 != value_2:
-               pass
+        leaf_1 = input_1.pre_order(input_1.tree)
+        help_1 = input_1.pre_order(input_1.tree)
+
+        leaf_2 = input_2.pre_order(input_2.tree)
+        help_2 = input_2.pre_order(input_2.tree)
+
+        stop_before = False
+        stop_after = False
+
+        try:
+            while True:
+                data_1 = next(leaf_1)
+                data_2 = next(leaf_2)
+
+                if not stop_before:
+                    next(help_1)
+                if not stop_after:
+                    next(help_2)
+                stop_before = False
+                stop_after = False
+
+                if data_1.content != data_2.content:
+                    help_data_1 = next(help_1)
+                    help_data_2 = next(help_2)
+
+                    if data_2.content == help_data_1.content:
+                        diff_tree.add_node(root, Node(f'- {data_1.content}', []))
+                        stop_after = True
+                        next(leaf_1)
+                    elif data_1.content == help_data_2.content:
+                        diff_tree.add_node(root, Node(f'+ {data_2.content}', []))
+                        stop_before = True
+                        next(leaf_2)
+                    elif data_1.content.split(':')[0] == data_2.content.split(':')[0]:
+                        diff_tree.add_node(root, Node(f'- {data_1.content}\n+ {data_2.content}', []))
+                        stop_after = True
+                        stop_before = True
+        except StopIteration:
+            pass
+        return diff_tree
 
     def research(self, object_, root, gen):
-        # if not isinstance(object_, (list, dict)):
-        #     yield object_
-        # else:
-        #     if isinstance(object_, list):
-        #         for node in object_:
-        #             for x in self.research(node):
-        #                 yield x
-        #     elif isinstance(object_, dict):
-        #         for key in object_.keys():
-        #             for x in self.research(object_[key]):
-        #                 yield x
-        #     else:
-        #         raise TypeError
-
         if not isinstance(object_, (list, dict)):
             gen.add_node(root, Node(object_, []))
             return
--- tests/manual_test_product_json.py
+++ tests/manual_test_product_json.py
@@ -4,11 +4,20 @@ from gendiff.generator_ast.generator_ast import GeneratorAST
 
 if __name__ == '__main__':
     an = JsonJSON()
-    gen = GeneratorAST()
-    with open('in.json', 'r') as file:
-        str_data = file.read()
-        des_data = an.read(str_data)
-        an.research(des_data, None, gen)
+    gen_before = GeneratorAST()
+    gen_after = GeneratorAST()
 
-        for i in gen.pre_order(gen.tree):
-            print(i)
+    with open('before.json', 'r') as before, open('after.json') as after:
+        str_data_before = before.read()
+        str_data_after = after.read()
+
+        des_data_before = an.read(str_data_before)
+        des_data_after = an.read(str_data_after)
+
+        an.research(des_data_before, None, gen_before)
+        an.research(des_data_after, None, gen_after)
+
+        # for i in gen_before.pre_order(gen_before.tree):
+        #     print(i)
+
+        an.compare(gen_before, gen_after)
