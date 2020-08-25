958598e8bf28f6799cd60777c17686df66891f10
Artem <bravo2694@gmail.com>
2020-08-25 16:46:48 +0300

    Add version number

--- gendiff/main.py
+++ gendiff/main.py
@@ -1,15 +1,16 @@
 import click
 from gendiff.parser import parse
+import pkg_resources
 
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
-# TODO find file this number version (in setup.py)
 def print_version(ctx, param, value):
     if not value or ctx.resilient_parsing:
         return
-    click.echo('Version 1.0')
+    version = pkg_resources.require("gendiff")[0].version
+    click.echo('Version ' + version)
     ctx.exit()
 
 
