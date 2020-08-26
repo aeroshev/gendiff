d6debb4bc13e9806359fa28f837c66c1e13477e0
Artem <bravo2694@gmail.com>
2020-08-26 17:25:36 +0300

    Add version packages in setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
+import os
 from setuptools import setup, find_packages
+
+
+def read(file_name: str):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
 
 
 setup(
     name='gendiff',
     version='1.0.0',
+    python_requires='>3.8.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click',
-        'PyYAML',
-        'Colorama'
+        'Click==7.1.2',
+        'PyYAML==5.3.1',
+        'Colorama==0.4.3'
     ],
     entry_points={
         'console_scripts': [
             'gendiff = gendiff.main:cli'
         ]
     },
+    download_url='https://github.com/aeroshev/gendiff.git',
     author='Artem',
     author_email='aeroshev@hsts.ru',
-    description='This program compare two files and show changes'
+    description='This program compare two files and show changes',
+    long_description=read('README')
 )
 
 
